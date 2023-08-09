14aa7c7c290fdd33aa246c3e27e912e1cb9586d7
Emilyguerrero <emilyguerrero59@gmail.com>
2023-08-09 15:34:28 -0400

    se crea la ruta raiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/Index'
-  get 'home/About'
-  get 'home/Contact'
+  get 'Home/Index' ,  to: 'Home#Index'
+  get 'Home/About' ,  to: 'Home#About'
+  get 'Home/Contact', to: 'Home#Contact'
+  root 'Home#Index'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
