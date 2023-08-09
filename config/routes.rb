9c72d01e2adc806ab883a51e23c7f7cdaa895a18
Emilyguerrero <emilyguerrero59@gmail.com>
2023-08-09 16:13:05 -0400

    se aplican estilos css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'Home/Index' ,  to: 'Home#Index'
-  get 'Home/About' ,  to: 'Home#About'
-  get 'Home/Contact', to: 'Home#Contact'
-  root 'Home#Index'
+  get 'home/Index' ,  to: 'home#Index'
+  get 'home/About' ,  to: 'home#About'
+  get 'home/Contact', to: 'home#Contact'
+  root 'home#Index'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
